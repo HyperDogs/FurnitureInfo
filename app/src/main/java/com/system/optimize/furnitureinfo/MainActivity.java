96b4f8564887e0a6cb5b05711a42dcf05d6ff7af
ton <ttonmaii@gmail.com>
2017-11-18 12:34:29 +0700

    add Create File

--- app/src/main/java/com/system/optimize/furnitureinfo/MainActivity.java
+++ app/src/main/java/com/system/optimize/furnitureinfo/MainActivity.java
@@ -2,29 +2,19 @@ package com.system.optimize.furnitureinfo;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.icu.text.UnicodeSetSpanner;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.FrameLayout;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
-import com.squareup.picasso.Picasso;
-import com.squareup.picasso.Target;
-
-import java.util.Locale;
+import java.io.File;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import resource.CreateFile;
 
 public class MainActivity extends Activity {
     //@BindView(R.id.changeLang_btn) Button changeLang_btn;
@@ -36,17 +26,40 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
-        /*Configuration config = new Configuration();
+         /*Configuration config = new Configuration();
         config.locale = new Locale("th");
         getResources().updateConfiguration(config, null);
-
         onCreate(null);*/
+
+        //ver Btn
+        ImageView mainCamera = (ImageView)findViewById(R.id.product_photo_imv);
+        mainCamera.setOnClickListener(onClickTakePic);
+
+
     }
 
     @OnClick(R.id.basic_info_block)
     public void changeLanguage_onClick() {
         Intent basicInfo = new Intent(MainActivity.this, BasicInfoActivity.class);
         startActivity(basicInfo);
+    }
+
+
+    private View.OnClickListener onClickTakePic = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            CreateFile createFile = new CreateFile();
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            File file = createFile.createUnique();
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+            startActivityForResult(intent, 1);
+
+        }
+    };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
     }
 }
